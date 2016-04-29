8d991ad79b241710434e53bd91dc70f24babb7ba
Artur Zakirov <a.zakirov@postgrespro.ru>
2016-04-29 11:49:33 +0300

    Fix keys comparition function

--- rum--1.0.sql
+++ rum--1.0.sql
@@ -50,7 +50,7 @@ AS
         OPERATOR        1       @@ (tsvector, tsquery),
         OPERATOR        2       @@@ (tsvector, tsquery),
         OPERATOR        3       >< (tsvector, tsquery) FOR ORDER BY pg_catalog.float_ops,
-        FUNCTION        1       bttextcmp(text, text),
+        FUNCTION        1       gin_cmp_tslexeme(text, text),
         FUNCTION        2       rum_extract_tsvector(tsvector,internal,internal,internal,internal),
         FUNCTION        3       rum_extract_tsquery(tsvector,internal,smallint,internal,internal,internal,internal),
         FUNCTION        4       gin_tsquery_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
@@ -59,4 +59,4 @@ AS
         FUNCTION        7       rum_tsvector_config(internal),
         FUNCTION        8       rum_tsquery_pre_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
         FUNCTION        9       rum_tsquery_distance(internal,smallint,tsvector,int,internal,internal,internal,internal,internal),
-        STORAGE         text;
+        STORAGE         tsvector;
