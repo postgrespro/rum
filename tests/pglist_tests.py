3283aed3b189157d5d2cc7c0abfe772e1c7232e8
Artur Zakirov <a.zakirov@postgrespro.ru>
2016-09-13 14:34:10 +0300

    autopep8 runned

--- tests/pglist_tests.py
+++ tests/pglist_tests.py
@@ -11,108 +11,111 @@ import gzip
 import testgres as tg
 
 if sys.version_info[0] < 3:
-	import urllib as request
+    import urllib as request
 else:
-	import urllib.request as request
+    import urllib.request as request
 
 from os.path import expanduser
 
+
 class PglistTests(unittest.TestCase):
-	def setUp(self):
-		self.node = tg.get_new_node("pglist_select")
-		try:
-			self.node.init()
-			self.node.append_conf("postgresql.conf",
-				"shared_buffers='4GB'\n"
-				"maintenance_work_mem='2GB'\n"
-				"max_wal_size='2GB'\n"
-				"work_mem='50MB'")
-			self.node.start()
 
-			self.init_pglist_data(self.node)
-		except Exception as e:
-			self.printlog(self.node.logs_dir + "/postgresql.log")
-			raise e
+    def setUp(self):
+        self.node = tg.get_new_node("pglist_select")
+        try:
+            self.node.init()
+            self.node.append_conf("postgresql.conf",
+                                  "shared_buffers='4GB'\n"
+                                  "maintenance_work_mem='2GB'\n"
+                                  "max_wal_size='2GB'\n"
+                                  "work_mem='50MB'")
+            self.node.start()
 
-	def tearDown(self):
-		tg.stop_all()
+            self.init_pglist_data(self.node)
+        except Exception as e:
+            self.printlog(self.node.logs_dir + "/postgresql.log")
+            raise e
 
-	def init_pglist_data(self, node):
-		# Check if 'pglist' base exists
-		base_exists = False
-		bases = node.execute("postgres", "SELECT datname FROM pg_database")
-		for base in bases:
-			if base[0].lower() == "pglist":
-				base_exists = True
-				break
+    def tearDown(self):
+        tg.stop_all()
 
-		if base_exists:
-			return
+    def init_pglist_data(self, node):
+        # Check if 'pglist' base exists
+        base_exists = False
+        bases = node.execute("postgres",
+                             "SELECT datname FROM pg_database WHERE datistemplate = false")
+        for base in bases:
+            if base[0].lower() == "pglist":
+                base_exists = True
+                break
 
-		# Check if 'pglist' dump exists
-		home = expanduser("~")
-		pglist_dump = os.path.join(home, "pglist-28-04-16.dump")
-		if not os.path.isfile(pglist_dump):
-			pglist_dumpgz = pglist_dump + ".gz"
-			if not os.path.isfile(pglist_dumpgz):
-				print("Downloading: %s" % pglist_dumpgz)
-				request.urlretrieve(
-					"http://www.sai.msu.su/~megera/postgres/files/pglist-28-04-16.dump.gz",
-					pglist_dumpgz)
+        if base_exists:
+            return
 
-			print("Decompressing: %s" % pglist_dumpgz)
-			gz = gzip.open(pglist_dumpgz, 'rb')
-			with open(pglist_dump, 'wb') as f:
-				f.write(gz.read())
+        # Check if 'pglist' dump exists
+        home = expanduser("~")
+        pglist_dump = os.path.join(home, "pglist-28-04-16.dump")
+        if not os.path.isfile(pglist_dump):
+            pglist_dumpgz = pglist_dump + ".gz"
+            if not os.path.isfile(pglist_dumpgz):
+                print("Downloading: %s" % pglist_dumpgz)
+                request.urlretrieve(
+                    "http://www.sai.msu.su/~megera/postgres/files/pglist-28-04-16.dump.gz",
+                    pglist_dumpgz)
 
-			os.remove(pglist_dumpgz)
+            print("Decompressing: %s" % pglist_dumpgz)
+            gz = gzip.open(pglist_dumpgz, 'rb')
+            with open(pglist_dump, 'wb') as f:
+                f.write(gz.read())
 
-		# Restore dump file
-		print("Restoring 'pglist'")
-		node.safe_psql("postgres", "CREATE DATABASE pglist")
-		node.psql("pglist", filename=pglist_dump)
+            os.remove(pglist_dumpgz)
 
-		node.safe_psql("pglist", "CREATE EXTENSION rum")
+        # Restore dump file
+        print("Restoring 'pglist'")
+        node.safe_psql("postgres", "CREATE DATABASE pglist")
+        node.psql("pglist", filename=pglist_dump)
 
-	def printlog(self, logfile):
-		with open(logfile, 'r') as log:
-			for line in log.readlines():
-				print(line)
+        node.safe_psql("pglist", "CREATE EXTENSION rum")
 
-	def test_order_by(self):
-		"""Tests SELECT constructions to 'pglist' base"""
-		try:
-			print("Creating index 'rumidx_orderby_sent'")
+    def printlog(self, logfile):
+        with open(logfile, 'r') as log:
+            for line in log.readlines():
+                print(line)
 
-			self.node.safe_psql(
-				"pglist",
-				"CREATE INDEX rumidx_orderby_sent ON pglist USING rum ("
-				"  fts rum_tsvector_timestamp_ops, sent) "
-				"  WITH (attach=sent, to=fts, order_by_attach=t)")
+    def test_order_by(self):
+        """Tests SELECT constructions to 'pglist' base"""
+        try:
+            print("Creating index 'rumidx_orderby_sent'")
 
-			print("Running tests")
+            self.node.safe_psql(
+                "pglist",
+                "CREATE INDEX rumidx_orderby_sent ON pglist USING rum ("
+                "  fts rum_tsvector_timestamp_ops, sent) "
+                "  WITH (attach=sent, to=fts, order_by_attach=t)")
 
-			self.assertEqual(
-				self.node.safe_psql(
-					"pglist",
-					"SELECT sent, subject "
-					"  FROM pglist "
-					"  WHERE fts @@ to_tsquery('english', 'backend <-> crushed') "
-					"  ORDER BY sent <=| '2016-01-01 00:01' LIMIT 5"
-				),
-				b'1999-06-02 11:52:46|Re: [HACKERS] PID of backend\n'
-			)
+            print("Running tests")
 
-			self.assertEqual(
-				self.node.safe_psql(
-					"pglist",
-					"SELECT count(*) FROM pglist WHERE fts @@ to_tsquery('english', 'tom & lane')"
-				),
-				b'222813\n'
-			)
-		except Exception as e:
-			self.printlog(self.node.logs_dir + "/postgresql.log")
-			raise e
+            self.assertEqual(
+                self.node.safe_psql(
+                    "pglist",
+                    "SELECT sent, subject "
+                    "  FROM pglist "
+                    "  WHERE fts @@ to_tsquery('english', 'backend <-> crushed') "
+                    "  ORDER BY sent <=| '2016-01-01 00:01' LIMIT 5"
+                ),
+                b'1999-06-02 11:52:46|Re: [HACKERS] PID of backend\n'
+            )
+
+            self.assertEqual(
+                self.node.safe_psql(
+                    "pglist",
+                    "SELECT count(*) FROM pglist WHERE fts @@ to_tsquery('english', 'tom & lane')"
+                ),
+                b'222813\n'
+            )
+        except Exception as e:
+            self.printlog(self.node.logs_dir + "/postgresql.log")
+            raise e
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
