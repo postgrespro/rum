6cc3a18a6454359b200258f1087f3266ebcc4b5f
Arthur Zakirov <z-arthur@yandex.ru>
2017-06-26 17:29:54 +0300

    Fix update script

--- gen_rum_sql--1.1--1.2.pl
+++ gen_rum_sql--1.1--1.2.pl
@@ -12,7 +12,7 @@ EOT
 my $opclass_distance_template=<<EOT;
 
 ALTER OPERATOR FAMILY rum_TYPEIDENT_ops USING rum ADD
-	FUNCTION	8	rum_TYPEIDENT_key_distance(internal,smallint,TYPENAME,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+	FUNCTION	8	(TYPENAME,TYPENAME) rum_TYPEIDENT_key_distance(internal,smallint,TYPENAME,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
 
 EOT
 
@@ -64,22 +64,44 @@ print <<EOT;
  * RUM version 1.2
  */
 
-ALTER OPERATOR FAMILY rum_tsvector_ops USING rum DROP
-	FUNCTION	8	(internal,smallint,tsvector,int,internal,internal,internal,internal,internal);
-
-ALTER OPERATOR FAMILY rum_tsvector_hash_ops USING rum DROP
-	FUNCTION	8	(internal,smallint,tsvector,int,internal,internal,internal,internal,internal);
+DROP OPERATOR CLASS rum_tsvector_ops USING rum;
+DROP OPERATOR CLASS rum_tsvector_hash_ops USING rum;
 
 CREATE OR REPLACE FUNCTION rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal)
 RETURNS float8
 AS 'MODULE_PATHNAME'
 LANGUAGE C IMMUTABLE STRICT;
 
-ALTER OPERATOR FAMILY rum_tsvector_ops USING rum ADD
-	FUNCTION	8	rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+CREATE OPERATOR CLASS rum_tsvector_ops
+DEFAULT FOR TYPE tsvector USING rum
+AS
+        OPERATOR        1       @@ (tsvector, tsquery),
+        OPERATOR        2       <=> (tsvector, tsquery) FOR ORDER BY pg_catalog.float_ops,
+        FUNCTION        1       gin_cmp_tslexeme(text, text),
+        FUNCTION        2       rum_extract_tsvector(tsvector,internal,internal,internal,internal),
+        FUNCTION        3       rum_extract_tsquery(tsquery,internal,smallint,internal,internal,internal,internal),
+        FUNCTION        4       rum_tsquery_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
+        FUNCTION        5       gin_cmp_prefix(text,text,smallint,internal),
+        FUNCTION        6       rum_tsvector_config(internal),
+        FUNCTION        7       rum_tsquery_pre_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
+        FUNCTION        8       rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal),
+        FUNCTION        10      rum_ts_join_pos(internal, internal),
+        STORAGE         text;
 
-ALTER OPERATOR FAMILY rum_tsvector_hash_ops USING rum ADD
-	FUNCTION	8	rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+CREATE OPERATOR CLASS rum_tsvector_hash_ops
+FOR TYPE tsvector USING rum
+AS
+        OPERATOR        1       @@ (tsvector, tsquery),
+        OPERATOR        2       <=> (tsvector, tsquery) FOR ORDER BY pg_catalog.float_ops,
+        FUNCTION        1       btint4cmp(integer, integer),
+        FUNCTION        2       rum_extract_tsvector_hash(tsvector,internal,internal,internal,internal),
+        FUNCTION        3       rum_extract_tsquery_hash(tsquery,internal,smallint,internal,internal,internal,internal),
+        FUNCTION        4       rum_tsquery_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
+        FUNCTION        6       rum_tsvector_config(internal),
+        FUNCTION        7       rum_tsquery_pre_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
+        FUNCTION        8       rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal),
+        FUNCTION        10      rum_ts_join_pos(internal, internal),
+        STORAGE         integer;
 
 EOT
 
--- rum--1.1--1.2.sql
+++ rum--1.1--1.2.sql
@@ -2,22 +2,44 @@
  * RUM version 1.2
  */
 
-ALTER OPERATOR FAMILY rum_tsvector_ops USING rum DROP
-	FUNCTION	8	(internal,smallint,tsvector,int,internal,internal,internal,internal,internal);
-
-ALTER OPERATOR FAMILY rum_tsvector_hash_ops USING rum DROP
-	FUNCTION	8	(internal,smallint,tsvector,int,internal,internal,internal,internal,internal);
+DROP OPERATOR CLASS rum_tsvector_ops USING rum;
+DROP OPERATOR CLASS rum_tsvector_hash_ops USING rum;
 
 CREATE OR REPLACE FUNCTION rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal)
 RETURNS float8
 AS 'MODULE_PATHNAME'
 LANGUAGE C IMMUTABLE STRICT;
 
-ALTER OPERATOR FAMILY rum_tsvector_ops USING rum ADD
-	FUNCTION	8	rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+CREATE OPERATOR CLASS rum_tsvector_ops
+DEFAULT FOR TYPE tsvector USING rum
+AS
+        OPERATOR        1       @@ (tsvector, tsquery),
+        OPERATOR        2       <=> (tsvector, tsquery) FOR ORDER BY pg_catalog.float_ops,
+        FUNCTION        1       gin_cmp_tslexeme(text, text),
+        FUNCTION        2       rum_extract_tsvector(tsvector,internal,internal,internal,internal),
+        FUNCTION        3       rum_extract_tsquery(tsquery,internal,smallint,internal,internal,internal,internal),
+        FUNCTION        4       rum_tsquery_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
+        FUNCTION        5       gin_cmp_prefix(text,text,smallint,internal),
+        FUNCTION        6       rum_tsvector_config(internal),
+        FUNCTION        7       rum_tsquery_pre_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
+        FUNCTION        8       rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal),
+        FUNCTION        10      rum_ts_join_pos(internal, internal),
+        STORAGE         text;
 
-ALTER OPERATOR FAMILY rum_tsvector_hash_ops USING rum ADD
-	FUNCTION	8	rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+CREATE OPERATOR CLASS rum_tsvector_hash_ops
+FOR TYPE tsvector USING rum
+AS
+        OPERATOR        1       @@ (tsvector, tsquery),
+        OPERATOR        2       <=> (tsvector, tsquery) FOR ORDER BY pg_catalog.float_ops,
+        FUNCTION        1       btint4cmp(integer, integer),
+        FUNCTION        2       rum_extract_tsvector_hash(tsvector,internal,internal,internal,internal),
+        FUNCTION        3       rum_extract_tsquery_hash(tsquery,internal,smallint,internal,internal,internal,internal),
+        FUNCTION        4       rum_tsquery_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
+        FUNCTION        6       rum_tsvector_config(internal),
+        FUNCTION        7       rum_tsquery_pre_consistent(internal,smallint,tsvector,int,internal,internal,internal,internal),
+        FUNCTION        8       rum_tsquery_distance(internal,smallint,internal,smallint,tsvector,int,internal,internal,internal,internal,internal,internal),
+        FUNCTION        10      rum_ts_join_pos(internal, internal),
+        STORAGE         integer;
 
 /*--------------------int2-----------------------*/
 
@@ -28,7 +50,7 @@ LANGUAGE C IMMUTABLE STRICT;
 
 
 ALTER OPERATOR FAMILY rum_int2_ops USING rum ADD
-	FUNCTION	8	rum_int2_key_distance(internal,smallint,int2,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+	FUNCTION	8	(int2,int2) rum_int2_key_distance(internal,smallint,int2,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
 
 /*--------------------int4-----------------------*/
 
@@ -39,7 +61,7 @@ LANGUAGE C IMMUTABLE STRICT;
 
 
 ALTER OPERATOR FAMILY rum_int4_ops USING rum ADD
-	FUNCTION	8	rum_int4_key_distance(internal,smallint,int4,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+	FUNCTION	8	(int4,int4) rum_int4_key_distance(internal,smallint,int4,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
 
 /*--------------------int8-----------------------*/
 
@@ -50,7 +72,7 @@ LANGUAGE C IMMUTABLE STRICT;
 
 
 ALTER OPERATOR FAMILY rum_int8_ops USING rum ADD
-	FUNCTION	8	rum_int8_key_distance(internal,smallint,int8,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+	FUNCTION	8	(int8,int8) rum_int8_key_distance(internal,smallint,int8,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
 
 /*--------------------float4-----------------------*/
 
@@ -61,7 +83,7 @@ LANGUAGE C IMMUTABLE STRICT;
 
 
 ALTER OPERATOR FAMILY rum_float4_ops USING rum ADD
-	FUNCTION	8	rum_float4_key_distance(internal,smallint,float4,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+	FUNCTION	8	(float4,float4) rum_float4_key_distance(internal,smallint,float4,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
 
 /*--------------------float8-----------------------*/
 
@@ -72,7 +94,7 @@ LANGUAGE C IMMUTABLE STRICT;
 
 
 ALTER OPERATOR FAMILY rum_float8_ops USING rum ADD
-	FUNCTION	8	rum_float8_key_distance(internal,smallint,float8,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+	FUNCTION	8	(float8,float8) rum_float8_key_distance(internal,smallint,float8,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
 
 /*--------------------money-----------------------*/
 
@@ -83,7 +105,7 @@ LANGUAGE C IMMUTABLE STRICT;
 
 
 ALTER OPERATOR FAMILY rum_money_ops USING rum ADD
-	FUNCTION	8	rum_money_key_distance(internal,smallint,money,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+	FUNCTION	8	(money,money) rum_money_key_distance(internal,smallint,money,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
 
 /*--------------------oid-----------------------*/
 
@@ -94,5 +116,5 @@ LANGUAGE C IMMUTABLE STRICT;
 
 
 ALTER OPERATOR FAMILY rum_oid_ops USING rum ADD
-	FUNCTION	8	rum_oid_key_distance(internal,smallint,oid,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
+	FUNCTION	8	(oid,oid) rum_oid_key_distance(internal,smallint,oid,smallint,tsvector,int,internal,internal,internal,internal,internal,internal);
 
