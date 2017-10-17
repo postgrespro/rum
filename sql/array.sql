09fe92a51479cbc13ebca8cc1e12c9566cc7f8a4
Dmitry Ivanov <ivadmi5@gmail.com>
2017-10-17 14:10:13 +0300

    more tests

--- sql/array.sql
+++ sql/array.sql
@@ -1,6 +1,10 @@
 set enable_seqscan=off;
 
 
+/*
+ * Complete checks for int2[].
+ */
+
 CREATE TABLE test_array (
 	i int2[]
 );
@@ -53,4 +57,46 @@ SELECT * FROM test_array WHERE i <@ '{1,2,3,4}';
 SELECT * FROM test_array WHERE i <@ '{4,3,2,1}';
 SELECT * FROM test_array WHERE i <@ '{0,0}';
 SELECT * FROM test_array WHERE i <@ '{100}';
+
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
+SELECT * FROM test_array WHERE i % '{}';
+SELECT * FROM test_array WHERE i % '{1}';
+SELECT * FROM test_array WHERE i % '{2}';
+SELECT * FROM test_array WHERE i % '{1,2}';
+SELECT * FROM test_array WHERE i % '{1,2,4}';
+SELECT * FROM test_array WHERE i % '{1,2,3,4}';
+SELECT * FROM test_array WHERE i % '{4,3,2,1}';
+SELECT * FROM test_array WHERE i % '{1,2,3,4,5}';
+SELECT * FROM test_array WHERE i % '{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}';
+SELECT * FROM test_array WHERE i % '{1,10,20,30,40,50}';
+SELECT * FROM test_array WHERE i % '{1,10,20,30}';
+SELECT * FROM test_array WHERE i % '{1,1,1,1,1}';
+SELECT * FROM test_array WHERE i % '{0,0}';
+SELECT * FROM test_array WHERE i % '{100}';
+
+
+/*
+ * Sanity checks for popular array types.
+ */
+
+ALTER TABLE test_array ALTER COLUMN i TYPE int4[];
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i && '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i @> '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i <@ '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
+
+ALTER TABLE test_array ALTER COLUMN i TYPE int8[];
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i && '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i @> '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i <@ '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
+
+ALTER TABLE test_array ALTER COLUMN i TYPE text[];
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i && '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i @> '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i <@ '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
 
