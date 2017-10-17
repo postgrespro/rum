cbe3732f05920f4432997929aa32a5d75b64926a
Dmitry Ivanov <ivadmi5@gmail.com>
2017-10-17 14:22:11 +0300

    more sanity checks for array_ops

--- sql/array.sql
+++ sql/array.sql
@@ -13,6 +13,10 @@ INSERT INTO test_array VALUES ('{}'), ('{0}'), ('{1,2,3,4}'), ('{1,2,3}'), ('{1,
 
 CREATE INDEX idx_array ON test_array USING rum (i);
 
+SELECT * FROM test_array WHERE i = '{NULL}';
+SELECT * FROM test_array WHERE i = '{1,2,3,NULL}';
+SELECT * FROM test_array WHERE i = '{{1,2},{3,4}}';
+
 EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
 SELECT * FROM test_array WHERE i = '{}';
 SELECT * FROM test_array WHERE i = '{0}';
@@ -94,6 +98,41 @@ EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i <@ '{}';
 EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
 
 ALTER TABLE test_array ALTER COLUMN i TYPE text[];
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i && '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i @> '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i <@ '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
+
+ALTER TABLE test_array ALTER COLUMN i TYPE varchar[];
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i && '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i @> '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i <@ '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
+
+ALTER TABLE test_array ALTER COLUMN i TYPE char[];
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i && '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i @> '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i <@ '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
+
+ALTER TABLE test_array ALTER COLUMN i TYPE numeric[] USING i::numeric[];
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i && '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i @> '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i <@ '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
+
+ALTER TABLE test_array ALTER COLUMN i TYPE float4[] USING i::float4[];
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i && '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i @> '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i <@ '{}';
+EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i % '{}';
+
+ALTER TABLE test_array ALTER COLUMN i TYPE float8[] USING i::float8[];
 EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i = '{}';
 EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i && '{}';
 EXPLAIN (COSTS OFF) SELECT * FROM test_array WHERE i @> '{}';
