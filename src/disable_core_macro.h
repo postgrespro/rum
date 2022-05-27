dc52c063e82f1ff33785b243f0878ac9ad92a69e
Pavel Borisov <pashkin.elfe@gmail.com>
2022-05-27 16:16:48 +0400

    Fix TRACE_START redefinition in PG10 -

--- src/disable_core_macro.h
+++ src/disable_core_macro.h
@@ -16,9 +16,17 @@
 #undef TRACE_POSTGRESQL_SORT_START
 #undef TRACE_POSTGRESQL_SORT_DONE
 
+#if PG_VERSION_NUM >= 110000
 #define TRACE_POSTGRESQL_SORT_START(arg1, arg2, arg3, arg4, arg5, arg6) \
+#else
+#define TRACE_POSTGRESQL_SORT_START(arg1, arg2, arg3, arg4, arg5) \
+#endif
 			do {} while(0)
+
+
 #define TRACE_POSTGRESQL_SORT_DONE(arg1, arg2) \
 			do {} while(0)
+
+
 
 #endif /* __DISABLE_CORE_MACRO_H__ */
